008e928c0daf754b8bdc0003d62110c6c719f764
dhanasekhar-k-e <k.e.dhanasekhar@gmail.com>
2020-11-11 15:44:38 +0530

    commit all

---
+++ index.js
@@ -0,0 +1,58 @@
+var paraText = [
+    "this is a simple paragraph that is meant to be nice and easy to type which is why there will be mommas no periods or any capital varters so i guess this means that it cannot really be considered a paragraph but just a series of run on sentences this should help you get faster at typing as im trying not to use too many difficult words in it although i think that i might start making it hard by including some more difficult varters I'm typing pretty quickly so forgive me for any mistakes i think that i will not just tell you a story about the time i went to the zoo and found a monkey and a fox playing together they were so cute and i think that they were not supposed to be in the same cage but they somehow were and i loved watching them horse around forgive the pun well i hope that it has been highly enjoyable typing this paragraph and i wish you the best of luck getting the best score that you possibly can"
+  ];
+var timerText = document.querySelector(".currTime");
+var accuracyText = document.querySelector(".currAccuracy");
+var errorText = document.querySelector(".currErrors");
+var cpmText = document.querySelector(".currCpm");
+var wpmText = document.querySelector(".currWpm");
+var quoteText = document.querySelector(".quote");
+var inputArea = document.querySelector(".inputArea");
+var restartBtn = document.querySelector(".restarBtn");
+var cpmGroup = document.querySelector(".cpm");
+var wpmGroup = document.querySelector(".wpm");
+var errorGroup = document.querySelector(".errors");
+var accuracyGroup = document.querySelector(".accuracy");
+
+
+function startTest(){
+
+}
+
+function endTest(){
+
+}
+
+function getEasyText(){
+
+}
+
+function getMediumText(){
+    
+}
+
+function getDifficultText(){
+    
+}
+
+function updateText(){
+
+}
+
+function process(){
+
+
+}
+
+function updateTimer(){
+    
+}
+
+let easybtn = document.getElementById("easy")
+easybtn.addEventListener("click", () => location.assign("./easy_text.html"))
+
+let mediumbtn = document.getElementById("medium")
+mediumbtn.addEventListener("click", () => location.assign("./medium_text.html"))
+
+let hardbtn = document.getElementById("hard")
+hardbtn.addEventListener("click", () => location.assign("./difficult_text.html"))
